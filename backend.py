1abffd56c104dde26fee273ad8d9c911b3ee8e57
Art.py <lekcjephp@gmail.com>
2012-02-21 15:08:04 +0000

    sprowadzenie do dzialania, poprawienie spacji

--- backend.py
+++ backend.py
@@ -3,21 +3,21 @@ import requests,sys,time,smtplib
 
 
 def wyslij_mail(toaddrs,msg, username, password):
- 	sourceAddress = username +"@gmail.com"
-	# The actual mail send  
-	server = smtplib.SMTP('smtp.gmail.com:587')  
-	server.starttls()  
-	server.login(username,password)  
-	server.sendmail(sourceAddress, toaddrs, msg)  
-	server.quit()
+        sourceAddress = username +"@gmail.com"
+        # The actual mail send  
+        server = smtplib.SMTP('smtp.gmail.com:587')  
+        server.starttls()  
+        server.login(username,password)  
+        server.sendmail(sourceAddress, toaddrs, msg)  
+        server.quit()
 
 def watcher(pageAddress, toaddrs,msg, username, password):
-	strona = requests.get(pageAddress)
-	startSize = len(strona.text)
-	while True:
-		strona = requests.get(pageAddress)
-		actualSize = len(stronaText)
-		time.sleep(10)
-		if actualSize != startSize:
-			wyslij_mail(toaddrs, msg, username, password )
-			return 1
+        strona = requests.get(pageAddress)
+        startSize = len(strona.text)
+        while True:
+               strona = requests.get(pageAddress)
+               actualSize = len(stronaText)
+               time.sleep(10)
+               if actualSize != startSize:
+                     wyslij_mail(toaddrs, msg, username, password )
+                     return 1
